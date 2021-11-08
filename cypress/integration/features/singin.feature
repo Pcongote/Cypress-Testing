318326c62a55cefc21798b96d46718d9485a4b14
Pablo Congote <pablo.congote@MacBook-Pro-de-alejandra.local>
2021-11-08 15:02:34 -0500

    finish modification to completed verifications in cucumber

--- cypress/integration/features/singin.feature
+++ cypress/integration/features/singin.feature
@@ -18,4 +18,43 @@ Feature: Test My Store page
           And I type "Joe" in the "first name" field
           And I type "Doe" in the "last name" field
           And I type "123465789" in the "password" field
+          And I Select "27" in the "Day" dropdown
+          And I Select "April" in the "Month" dropdown
+          And I Select "1990" in the "Year" dropdown
+          And I type "Joe" in the "first name Adress" field
+          And I type "Doe" in the "last name Adress" field
+          And I type "False company" in the "company" field
+          And I type "False street 123" in the "aderess" field
+          And I type "Miami" in the "city" field
+          And I Select "Florida" in the "state" dropdown
+          And I type "33101" in the "zip code" field
+          And I type "Information XXXXXXXXXXXX" in the "information" field
+          And I type "1234567890" in the "phone" field
+          And I type "10987654321" in the "mobile phone" field
+          And I click the "register" button
+          And I type "shirt" in the "search" field
+          And I click the "lens" button
+          And I choose the "special yellow dress" button
+          And I click the "black color" button
+          And I click the "Add to car" button
+          And I click the "continue shopping" button
+          And I type "Dress" in the "search" field
+          And I click the "lens" button
+          And I add to cart "printed dress"
+          And I click the "Add to car" button
+          And I click the "Proceed to checkout" button
+          And I wanna "eliminate" the first option in the checkout
+          And I click the "next step" button
+          And I click the "next step" button
+          And I click the "I agree to the terms of service" button
+          And I click the "next step" button
+          And I click the "pay by bank wire" button
+          And I click the "next step" button
+          Then I expect to see the price "52.99"
+
           
+          
+
+          
+
+
--- cypress/integration/step_definitions/singin.js
+++ cypress/integration/step_definitions/singin.js
@@ -10,70 +10,99 @@ const selectors = {
     "gender": "#id_gender1",
     "first name": "#customer_firstname",
     "last name":  "#customer_lastname",
-    "password": "#passwd"
+    "password": "#passwd",
+    "Day": "#days",
+    "Month": "#months",
+    "Year": "#years",
+    "first name Adress": "#firstname",
+    "last name Adress": "#lastname",
+    "company": "#company",
+    "aderess": "#address1",
+    "city": "#city",
+    "state": "#id_state",
+    "zip code": "#postcode",
+    "information": "#other",
+    "phone": "#phone",
+    "mobile phone": "#phone_mobile",
+    "register": "#submitAccount > span",
+    "search": "#search_query_top",
+    "lens": "#searchbox > .btn",
+    "special yellow dress": "Printed Summer Dress",
+    "black color": "#color_11",
+    "Add to car": ".exclusive > span",
+    "continue shopping": ".continue > span",
+    "printed dress": '[title="Printed Dress"]',
+    "Proceed to checkout": ".button-medium > span",
+    "eliminate": ".icon-trash",
+    "next step": ".cart_navigation > .button > span",
+    "I agree to the terms of service": "#cgv",
+    "pay by bank wire": ".bankwire > span"
+    
+
+
 }
 
-Given('I open the login page and complete the sign in process', () => {
-    cy.visit(url)
-    cy.get('.login').click()
-    cy.get('#email_create').click().type('falseff_' + Math.floor(Math.random() * 1000) + 1 + '@gmail.com')
-    cy.get('#SubmitCreate > span').click()
-    cy.get('#id_gender1').click()
-    cy.get('#customer_firstname').type('Joe')
-    cy.get('#customer_lastname').type('Doe')
-    cy.get('#passwd').type('123456798')
-    cy.get('#days').select('27')
-    cy.get('#months').select('April')
-    cy.get('#years').select('1950')
-    cy.get('#firstname').type('Joe')
-    cy.get('#lastname').type('Doe')
-    cy.get('#company').type('false Job')
-    cy.get('#address1').type('False street 123')
-    cy.get('#city').type('Miami')
-    cy.get('#id_state').select('Florida')
-    cy.get('#postcode').type('33101')
-    cy.get('#other').type('Information XXXXXXXXXXXX')
-    cy.get('#phone').type('123456894')
-    cy.get('#phone_mobile').type('123456123')
-    cy.get('#submitAccount > span').click()
-    cy.screenshot()
-})
+// Given('I open the login page and complete the sign in process', () => {
+//     cy.visit(url)
+//     cy.get('.login').click()
+//     cy.get('#email_create').click().type('falseff_' + Math.floor(Math.random() * 1000) + 1 + '@gmail.com')
+//     cy.get('#SubmitCreate > span').click()
+//     cy.get('#id_gender1').click()
+//     cy.get('#customer_firstname').type('Joe')
+//     cy.get('#customer_lastname').type('Doe')
+//     cy.get('#passwd').type('123456798')
+//     cy.get('#days').select('27')
+//     cy.get('#months').select('April')
+//     cy.get('#years').select('1950')
+//     cy.get('#firstname').type('Joe')
+//     cy.get('#lastname').type('Doe')
+//     cy.get('#company').type('false Job')
+//     cy.get('#address1').type('False street 123')
+//     cy.get('#city').type('Miami')
+//     cy.get('#id_state').select('Florida')
+//     cy.get('#postcode').type('33101')
+//     cy.get('#other').type('Information XXXXXXXXXXXX')
+//     cy.get('#phone').type('123456894')
+//     cy.get('#phone_mobile').type('123456123')
+//     cy.get('#submitAccount > span').click()
+//     cy.screenshot()
+// })
 
 Given('I open the login page', () => {
     cy.visit(url)
 })
 
-When('I add two products and remove one', () => {
-    cy.get('#search_query_top').type('shirt')
-    cy.get('#searchbox > .btn').click()
-    cy.contains('Printed Summer Dress').click()
-    cy.get('#color_11').click()
-    cy.screenshot()
-    cy.get('.exclusive > span').click()
-    cy.wait(8000)
-    cy.get('.continue > span').click()
-    cy.get('#search_query_top').type('jean')
-    cy.get('#searchbox > .btn').click()
-    cy.get('#search_query_top').clear()
-    cy.get('#search_query_top').type('Dress')
-    cy.get('#searchbox > .btn').click()
-    cy.get('[title="Printed Dress"]').eq(1).click()
-    cy.screenshot()
-    cy.get('.exclusive > span').click()
-    cy.wait(8000)
-    cy.get('.button-medium > span').click()
-    cy.get('.icon-trash').eq(0).click()
-    cy.wait(3000)
-})
+// When('I add two products and remove one', () => {
+//     cy.get('#search_query_top').type('shirt')
+//     cy.get('#searchbox > .btn').click()
+//     cy.contains('Printed Summer Dress').click()
+//     cy.get('#color_11').click()
+//     cy.screenshot()
+//     cy.get('.exclusive > span').click()
+//     cy.wait(8000)
+//     cy.get('.continue > span').click()
+//     cy.get('#search_query_top').type('jean')
+//     cy.get('#searchbox > .btn').click()
+//     cy.get('#search_query_top').clear()
+//     cy.get('#search_query_top').type('Dress')
+//     cy.get('#searchbox > .btn').click()
+//     cy.get('[title="Printed Dress"]').eq(1).click()
+//     cy.screenshot()
+//     cy.get('.exclusive > span').click()
+//     cy.wait(8000)
+//     cy.get('.button-medium > span').click()
+//     cy.get('.icon-trash').eq(0).click()
+//     cy.wait(3000)
+// })
 
-When('I complete the purchase', () => {
-    cy.get('.cart_navigation > .button > span').click()
-    cy.get('.cart_navigation > .button > span').click()
-    cy.get('#cgv').click()
-    cy.get('.cart_navigation > .button > span').click()
-    cy.get('.bankwire > span').click()
-    cy.get('#cart_navigation > .button > span').click()
-})
+// When('I complete the purchase', () => {
+//     cy.get('.cart_navigation > .button > span').click()
+//     cy.get('.cart_navigation > .button > span').click()
+//     cy.get('#cgv').click()
+//     cy.get('.cart_navigation > .button > span').click()
+//     cy.get('.bankwire > span').click()
+//     cy.get('#cart_navigation > .button > span').click()
+// })
 
 When('I click the {string} button', (elementName) => {
     cy.get(selectors[elementName]).click()
@@ -88,6 +117,23 @@ When('I type {string} in the {string} field', (text, elementName) => {
     else {
         cy.get(selectors[elementName]).type(text)
     }
+})
+
+When ('I Select {string} in the {string} dropdown', (text, elementName)=>{
+    cy.get(selectors[elementName]).select(text)
+})
+
+When ("I choose the {string} button", (elementName) => {
+    cy.contains(selectors[elementName]).click()
+
+})
+
+When ("I add to cart {string}", (elementName) => {
+    cy.get(selectors[elementName]).eq(1).click()
+})
+
+When("I wanna {string} the first option in the checkout", (elementName) => {
+    cy.get(selectors[elementName]).eq(0).click()
 })
 
 Then('I expect to see the price {string}', (price) => {
